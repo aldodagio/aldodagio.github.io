bdab0e01e8c5b6342885fc9a272659d11aa9d1d7
aldodagio <103005119+aldodagio@users.noreply.github.com>
2022-10-31 15:06:32 -0700

    initial commit'

---
+++ src/main/java/com/example/SportyApplication/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.SportyApplication;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Customer Instance";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><form><body>");
+        out.println("<label for=\"team\"> Enter the sports team you want to watch: </label>");
+        out.println("<input type=\"text\" id=\"team\" name=\"team\"><br><br>");
+        out.println("<label for=\"zip\"> Enter your ZIP code: </label>");
+        out.println("<input type=\"text\" id=\"zip\" name=\"zip\"><br><br>");
+        out.println("</body></form></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sporty</title>
+</head>
+<body>
+<h1><%= "Sporty Prototype" %>
+</h1>
+<br/>
+<a href="hello-servlet">Find Restaurants Playing Your Sports Team Game In Your Area (within 0-50 miles)</a><br>
+<a href="RestaurantServlet">Restaurant Sport Package Editor</a>
+</body>
+</html>
