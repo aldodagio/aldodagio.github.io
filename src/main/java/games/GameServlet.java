3adc0315b26651b17af403a5fbb14f70af95832c
aldodagio <103005119+aldodagio@users.noreply.github.com>
2022-11-14 23:10:49 -0800

    Trying to figure out how to send answers back through in str.value in get request. It seems like it is too late once we are in onload function.

--- src/main/java/games/GameServlet.java
+++ src/main/java/games/GameServlet.java
@@ -23,7 +23,7 @@ public class GameServlet extends HttpServlet {
         out.println("<html><head>\n" +
                 "    <title>Package Editor</title>\n" +
                 "</head><form id=\"sportsListings\"><body>");
-        out.println("<input type=\"hidden\" id=\"lineupId\" name=\"lineupId\" value=\"USA-TX42500-X\">\n" +
+        /*out.println("<input type=\"hidden\" id=\"lineupId\" name=\"lineupId\" value=\"USA-TX42500-X\">\n" +
                 "    <input type=\"hidden\" id=\"startDateTime\" name=\"startDateTime\" value=\"2022-11-13\">\n" +
                 "    <input type=\"hidden\" id=\"api_key\" name=\"api_key\" value=\"32gwu9v9zkpr42bjzx8v9u8w\">");
         out.println("<script>function sendData() { const XHR = new XMLHttpRequest(); const FD = new FormData(form); XHR.open(\"GET\", \"http://data.tmsapi.com/v1.1/sports/59/events/airings?lineupId=USA-TX42500-X&startDateTime=2022-11-13T20%3A30Z&api_key=32gwu9v9zkpr42bjzx8v9u8w\"); XHR.send(FD); XHR.onload = () => { if(XHR.readyState === 4) { if(XHR.status === 200) { var res = JSON.parse(XHR.responseText); " +
@@ -36,7 +36,7 @@ public class GameServlet extends HttpServlet {
                 "window.location.replace(\"Success.jsp\"); } } }}const form = document.getElementById(\"sportsListings\");form.addEventListener(\"submit\", (event) => {\n" +
                 "      event.preventDefault();\n" +
                 "      sendData();\n" +
-                "    });</script>");
+                "    });</script>");*/
         out.println("<button type=\"submit\">Submit</button>");
         out.println("</body><form></html><br>");
 
